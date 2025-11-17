86f9dd537f26d8e374624e68096ca3e31cc8567e
Oxidi-source <systemadmin@primegroup.com.ph>
2025-11-17 17:03:42 +0800

    Update footer.jsx

--- src/config/footer.jsx
+++ src/config/footer.jsx
@@ -1,4 +1,5 @@
 import react from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -28,39 +29,73 @@ const Footer = () => {
         <div className="flex justify-center space-x-12 md:order-2 mt-8 md:mt-0">
           {/* About */}
           <div className="flex flex-col items-start ">
-            <p className="text-gray-500 dark:text-gray-400 hover:text-gray-600 text-lg dark:hover:text-gray-300 mb-2 font-semibold">
-              About
-            </p>
-            <a
-              href="#"
-              className="text-gray-400 text-sm hover:text-gray-500 dark:hover:text-gray-300"
+            <Link
+              to="/about"
+              className="text-gray-500 dark:text-gray-400 hover:text-gray-600 text-lg dark:hover:text-gray-300 mb-2 font-semibold"
             >
-              Our Story
-            </a>
-            <a
-              href="#"
-              className="text-gray-400 text-sm hover:text-gray-500 dark:hover:text-gray-300"
-            >
-              Team
-            </a>
+              About Us
+            </Link>
           </div>
 
           {/* Products */}
           <div className="flex flex-col items-start">
             <p className="text-gray-500 dark:text-gray-400 hover:text-gray-600 text-lg dark:hover:text-gray-300 mb-2 font-semibold">
-              Products
+              Solutions
             </p>
             <a
-              href="#"
+              href="/solutions/mhe"
               className="text-gray-400 text-sm hover:text-gray-500 dark:hover:text-gray-300"
             >
-              Solutions
+              Material Handling Equipment
             </a>
             <a
-              href="#"
+              href="/solutions/industrial-storage"
               className="text-gray-400 text-sm hover:text-gray-500 dark:hover:text-gray-300"
             >
-              Services
+              Industrial Storage Solutions
+            </a>
+            <a
+              href="/solutions/cold-chain"
+              className="text-gray-400 text-sm hover:text-gray-500 dark:hover:text-gray-300"
+            >
+              Cold Chain Solutions
+            </a>
+
+            <a
+              href="/solutions/docks-and-doors"
+              className="text-gray-400 text-sm hover:text-gray-500 dark:hover:text-gray-300"
+            >
+              Docks and Doors
+            </a>
+            <a
+              href="/solutions/plastic-pallets,-bins-and-crates"
+              className="text-gray-400 text-sm hover:text-gray-500 dark:hover:text-gray-300"
+            >
+              Plastic Pallets, Bins and Crates
+            </a>
+            <a
+              href="/solutions/industrial-batteries-and-chargers"
+              className="text-gray-400 text-sm hover:text-gray-500 dark:hover:text-gray-300"
+            >
+              Industrial Batteries and Chargers
+            </a>
+            <a
+              href="/solutions/automation-solutions"
+              className="text-gray-400 text-sm hover:text-gray-500 dark:hover:text-gray-300"
+            >
+              Automation Solutions
+            </a>
+            <a
+              href="/solutions/warehouse-management-system"
+              className="text-gray-400 text-sm hover:text-gray-500 dark:hover:text-gray-300"
+            >
+              Warehouse Management Solutions
+            </a>
+            <a
+              href="/solutions/commercial-solutions"
+              className="text-gray-400 text-sm hover:text-gray-500 dark:hover:text-gray-300"
+            >
+              Commercial Solutions
             </a>
           </div>
 

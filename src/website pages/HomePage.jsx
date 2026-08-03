ad35bf63a07e4d84a79e179a536b4e160eef99f6
Allen-pogi <systemadmin@primegroup.com.ph>
2026-08-03 14:12:48 +0800

    update

--- src/website pages/HomePage.jsx
+++ src/website pages/HomePage.jsx
@@ -90,7 +90,7 @@ const WebsiteMain = () => {
         {/* Hero Section */}
         <div className="relative w-full h-screen overflow-hidden">
           <video
-            src="/bg4.mp4"
+            src="/bg7.mp4"
             autoPlay
             loop
             muted
